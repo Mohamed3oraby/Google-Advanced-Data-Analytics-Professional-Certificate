7c1ca058583be167d66d859abce1e6b815655dd4
Mohamed Oraby <Mohamed.3oraby@outlook.com>
2023-06-09 15:51:37 +0300

    Adding Task 1 and solving task 1a
    assigning two new variables with a list of states and counties on it

---
+++ Activity_ Lists & tuples.py
@@ -0,0 +1,17 @@
+## Task 1: Create lists and use tuples
+
+## Create lists
+# You'll work with state names and county names indicated below.
+# state_name	county_name
+# Arizona	    Maricopa
+# California	Alameda
+# California	Sacramento
+# Kentucky	    Jefferson
+# Louisiana	    East Baton Rouge
+
+# In this task, assign two variables:
+#     1. `state_names` - a `list` of each state in the `state_name` column in the table above, in order, as strings
+#     2. `county_names` - a `list` of each county in the `county_name` column in the table above, in order, as strings
+
+state_names = ["Arizona", "California", "California", "Kentucky", "Louisiana"]
+county_names = ["Maricopa", "Alameda", "Sacramento", "Jefferson", "East Baton Rouge"]
